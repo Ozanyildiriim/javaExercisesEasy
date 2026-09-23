ece40fde0e0c90b526b7ce0164266521f2f94ed4
Ozan <ozanyildirim@sabanciuniv.edu>
2026-09-23 20:38:23 +0300

    Day 8 - Chapter 7 / Challenges

---
+++ src/Chapter7OopClassesAndInherıtance/Challenges/Carpet.java
@@ -0,0 +1,17 @@
+package Chapter7OopClassesAndInherıtance.Challenges;
+
+public class Carpet {
+
+    private double cost;
+
+    public Carpet(double cost) {
+        if (cost < 0) {
+            cost = 0;
+        }
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
